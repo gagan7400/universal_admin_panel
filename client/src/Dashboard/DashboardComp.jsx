1bbcda91ff1ab164046d5671c6e4d8cba1b76097
Gagan <vishwakarmagagan24@gmail.com>
2025-06-24 10:06:47 +0530

    df

--- client/src/Dashboard/DashboardComp.jsx
+++ client/src/Dashboard/DashboardComp.jsx
@@ -1,16 +1,11 @@
 import React from 'react'
+import MonthlySalesChart from './MonthlySalesChart'
 
 export default function DashboardComp() {
     return (
         <div className='w-full h-full'>
-            <div className="flex flex-wrap items-center justify-between  gap-5">
-                <div className="w-50 p-5 flex-grow h-40 rounded-xl text-amber-50 bg-emerald-600">hello</div>
-                <div className="w-50 p-5 flex-grow h-40 rounded-xl text-amber-50 bg-emerald-600">hello</div>
-                <div className="w-50 p-5 flex-grow h-40 rounded-xl text-amber-50 bg-emerald-600">hello</div>
-                <div className="w-50 p-5 flex-grow h-40 rounded-xl text-amber-50 bg-emerald-600">hello</div>
-                <div className="w-50 p-5 flex-grow h-40 rounded-xl text-amber-50 bg-emerald-600">hello</div>
-                <div className="w-50 p-5 flex-grow h-40 rounded-xl text-amber-50 bg-emerald-600">hello</div>
-            </div>
+            <MonthlySalesChart />
+
         </div>
     )
 }
