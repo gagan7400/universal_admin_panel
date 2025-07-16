821eb4e45a5080fa9e63546ee238c8c9d78f67cc
Gagan <vishwakarmagagan24@gmail.com>
2025-07-16 10:36:19 +0530

    hh

--- client/src/protectedroute/ProtectedRoute.jsx
+++ client/src/protectedroute/ProtectedRoute.jsx
@@ -2,29 +2,69 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Navigate, useLocation } from 'react-router-dom';
-import { loadAdmin } from '../redux/actions/authActions.js';
+import { logout, loginSuccess, setAuthLoading } from '../redux/actions/authActions.js';
 import axios from 'axios';
 import Loader from '../layout/Loader.jsx';
 
-const ProtectedRoute = ({ children, allowedRoles = [] }) => {
+const ProtectedRoute = ({ children }) => {
     const dispatch = useDispatch();
     const location = useLocation();
-    const { isAuthenticated, loading, admin } = useSelector((state) => state.auth);
+    const { isAuthenticated, loading, error } = useSelector((state) => state.auth);
 
     useEffect(() => {
-        loadAdmin()
+        const checkAuth = async () => {
+            dispatch(setAuthLoading());
+            try {
+                const { data } = await axios.get("http://localhost:4000/api/admin/profile", {
+                    withCredentials: true,
+                });
+                if (data.success) {
+                    dispatch(loginSuccess(data.data));
+                } else {
+                    dispatch(logout());
+                }
+            } catch (error) {
+                dispatch(logout());
+            }
+        };
+        checkAuth();
     }, [dispatch]);
+    if (loading) { return <Loader /> };
 
-    if (loading) return <Loader />;
-
-    if (!isAuthenticated) return <Navigate to="/login" replace />;
-
-    // Role-based restriction
-    if (allowedRoles.length > 0 && !allowedRoles.includes(admin?.role)) {
-        return <Navigate to="/login" replace />;
-    }
+    if (!isAuthenticated) return <Navigate to="/login" state={{ from: location }} replace />;
 
     return children;
 };
 
 export default ProtectedRoute;
+
+// // ProtectedRoute.jsx
+// import React, { useEffect } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { Navigate, useLocation } from 'react-router-dom';
+// import { loadAdmin } from '../redux/actions/authActions.js';
+// import axios from 'axios';
+// import Loader from '../layout/Loader.jsx';
+
+// const ProtectedRoute = ({ children, allowedRoles = [] }) => {
+//     const dispatch = useDispatch();
+//     const location = useLocation();
+//     const { isAuthenticated, loading, admin } = useSelector((state) => state.auth);
+
+//     useEffect(() => {
+//         loadAdmin()
+//     }, [dispatch]);
+
+//     if (loading) return <Loader />;
+
+//     if (!isAuthenticated) return <Navigate to="/login" replace />;
+
+//     // // Role-based restriction
+//     // if (allowedRoles.length > 0 && !allowedRoles.includes(admin?.role)) {
+//     //     return <Navigate to="/login" replace />;
+//     // }
+
+//     return children;
+// };
+
+// export default ProtectedRoute;
