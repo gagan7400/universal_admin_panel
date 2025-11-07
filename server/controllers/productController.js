d72e310a582c297526579b99c9232e526e927b95
Gagan <vishwakarmagagan24@gmail.com>
2025-11-07 21:24:39 +0530

    ui update

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -13,7 +13,7 @@ var url = require('url');
 
 const base_url = process.env.NODE_ENV == "production" ? process.env.BASE_URL_LIVE : process.env.BASE_URL;
 // const base_url = "http://srv918880.hstgr.cloud:4000";
- 
+
 
 const createProductController = catchAsyncErrors(async (req, res, next) => {
 
@@ -311,4 +311,4 @@ const getProductsByCategory = async (req, res) => {
 module.exports = { updateProduct };
 
 
-module.exports = {    getAllCategories, getProductsByCategory, createProductController, getAllProducts, getProductDetails, deleteProduct, updateProduct, countProduct, addBanners, getBanners, deleteBannerImage }
+module.exports = { getAllCategories, getProductsByCategory, createProductController, getAllProducts, getProductDetails, deleteProduct, updateProduct, countProduct, addBanners, getBanners, deleteBannerImage }
