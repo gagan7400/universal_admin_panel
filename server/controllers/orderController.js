9f73207af2d2339feaea908a1e6c25ac7bbaa1e5
Gagan <vishwakarmagagan24@gmail.com>
2025-11-11 23:56:15 +0530

    my orders

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -89,7 +89,7 @@ exports.getSingleOrder = catchAsyncErrors(async (req, res, next) => {
 
 // get logged in user  Orders
 exports.myOrders = catchAsyncErrors(async (req, res, next) => {
-    const orders = await Order.find({ user: req.user._id });
+    const orders = await Order.find({ "user.id": req.user._id });
 
     res.status(200).json({
         success: true,
